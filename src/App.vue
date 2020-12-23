b23daabd1ebcfdc0d5a74da86f5244cdb966eda0
Maxim Klochkov <m.klochkov@sap.com>
2020-12-23 20:58:37 +0300

    Adding Modal component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ title }}</h1>
+    <p>Welcome...</p>
+    <div v-if="showModal">
+        <Modal theme="sale" @close="toggleModal">
+            <h1>{{ header }}</h1>
+            <p>{{ text }}</p>
+            <template v-slot:links>
+                <a href="#">Sign up</a>
+                <a href="#">More info</a>
+            </template>
+        </Modal>
+    </div>
+    <button @click="toggleModal">Open Modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: { Modal },
+    data() {
+        return {
+            title: 'My First Vue App :)',
+            header: 'Sign up for the Giveaway!',
+            text: 'Grab your Ninja swag for a half price!',
+            showModal: false
+        }
+    },
+    methods: {
+        toggleModal() {
+            this.showModal = !this.showModal
+        }
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+h1 {
+    display: inline-block;
+    border-bottom: 1px solid #ddd;
+    padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ 'sale': theme === 'sale'}">
+            <slot>Default content</slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'theme'
+    ],
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style>
+    .modal {
+        margin: 100px auto;
+        padding: 20px;
+
+        width: 400px;
+
+        background: white;
+        border-radius: 10px;
+    }
+
+    .backdrop {
+        position: fixed;
+        top: 0;
+
+        width: 100%;
+        height: 100%;
+
+        background: rgba(0,0,0,0.5);
+    }
+
+    h1 {
+        color: #03cfb4;
+
+        border: none;
+    }
+
+    .modal.sale {
+        background: crimson;
+        color: white;
+    }
+
+    .modal .actions {
+        margin: 30px 0 10px 0;
+
+        text-align: center;
+    }
+
+    .modal .actions a {
+        margin: 10px;
+        padding: 8px;
+
+        color: #333;
+        text-decoration: none;
+
+        border: 1px solid #eee;
+        border-radius: 4px;
+    }
+
+    .modal.sale h1 {
+        color: white;
+    }
+
+    .modal.sale .actions {
+        color: white;
+    }
+
+    .modal.sale .actions a {
+        color: white;
+    }
+</style>
